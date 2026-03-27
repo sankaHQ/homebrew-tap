86c8c223195fc4f91b3f4fe4eef5dd072f083c53
Haegwan Kim <haegwankim@gmail.com>
2026-03-28 06:56:23 +0900

    feat: add sanka formula

---
+++ Formula/sanka.rb
@@ -0,0 +1,19 @@
+class Sanka < Formula
+  include Language::Python::Virtualenv
+
+  desc "Thin command-line wrapper for the Sanka API"
+  homepage "https://github.com/haegwan/sanka-cli"
+  url "https://github.com/haegwan/sanka-cli/releases/download/v0.1.0/sanka_cli-0.1.0.tar.gz"
+  sha256 "0b3200eda854edaefb98217ba55361b2ce43b7a6593f934dead289950039783c"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match "No access token configured", shell_output("#{bin}/sanka auth status 2>&1", 1)
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Homebrew Tap for Sanka CLI
+
+Install the tap and formula:
+
+```bash
+brew tap haegwan/tap
+brew install haegwan/tap/sanka
+```
+
+This tap publishes the generated `sanka.rb` formula from the
+[`haegwan/sanka-cli`](https://github.com/haegwan/sanka-cli) release process.
